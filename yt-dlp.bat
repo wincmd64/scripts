9db207290a8ed020f651ae8f0781d2fe6a9ff086
sleepwalker <104612222+wincmd64@users.noreply.github.com>
2025-09-07 20:21:22 +0300

    Update yt-dlp.bat

--- yt-dlp.bat
+++ yt-dlp.bat
@@ -1,19 +1,20 @@
 :: Downloads video from link in clipboard using yt-dlp (+ ffmpeg)
+:: by github.com/wincmd64
 
 @echo off
 :: gets downloads folder path
 for /f "delims=" %%a in ('powershell -command "(New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path"') do set "DOWNLOADS=%%a"
-:: gets value from clipboard
-for /f "delims=" %%i in ('powershell Get-Clipboard') do set "url=%%i"
+:: gets URL, priority: parameter > clipboard
+if "%~1" neq "" (set "url=%~1") else (for /f "delims=" %%i in ('powershell Get-Clipboard') do set "url=%%i")
 :: gets yt-dlp path
 for /f "tokens=* delims=" %%a in ('where yt-dlp.exe 2^>nul') do set "app=%%a"
-if not exist "%app%" (color 4 & echo. & echo  yt-dlp not found. Try: winget install yt-dlp.yt-dlp & echo. & pause & exit) else (TITLE %app%)
+if not exist "%app%" (color C & echo. & echo  yt-dlp not found. Try: winget install yt-dlp.yt-dlp & echo. & pause & exit) else (TITLE %app%)
 :check
 yt-dlp.exe -F -S vext "%url%"
 if ERRORLEVEL 1 (
-	:: if the url is not read from the buffer - enter manually
+	:: if the url is invalid - enter manually
+    echo.
 	set /p url=Enter the url: 
-	echo.
 	goto check
 )
 echo.
@@ -31,6 +32,4 @@ if not defined num (
 	yt-dlp.exe %num% "%url%" -P "%DOWNLOADS%" -o "%%(title).50s.%%(ext)s" --no-part
 	if ERRORLEVEL 1 (goto check)
 )
-color 27
-explorer "%DOWNLOADS%"
-timeout 1
+color A & timeout 2 & explorer "%DOWNLOADS%"
