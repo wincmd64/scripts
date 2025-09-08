476699a7746e1b0fc4b7f879082da417b56ae92d
sleepwalker <104612222+wincmd64@users.noreply.github.com>
2025-09-08 12:44:40 +0300

    Add files via upload

--- pathman.ps1
+++ pathman.ps1
@@ -6,30 +6,17 @@ if (-not (Test-Path -Path $Path)) { Write-Warning "The directory '$Path' does no
 
 # Get current User PATH
 $currentPath = [System.Environment]::GetEnvironmentVariable('PATH', 'User')
-$arrPath = @()
-if (-not [string]::IsNullOrEmpty($currentPath)) {
-    $arrPath = $currentPath -split [IO.Path]::PathSeparator
-}
-
-# Check if path exists
-$exists = $false
-foreach ($item in $arrPath) {
-    if ($item.TrimEnd('\') -eq $Path.TrimEnd('\')) {
-        $exists = $true
-        break
-    }
-}
+# Split PATH into array
+$arrPath = if ([string]::IsNullOrEmpty($currentPath)) { @() } else { $currentPath -split [IO.Path]::PathSeparator }
+# Check if already exists + removing trailing backslashes
+$exists = ($arrPath | ForEach-Object TrimEnd('\')) -contains $Path.TrimEnd('\')
 
 if ($exists) {
-    # Remove all occurrences
     $newPath = ($arrPath | Where-Object { $_.TrimEnd('\') -ne $Path.TrimEnd('\') }) -join [IO.Path]::PathSeparator
-    [System.Environment]::SetEnvironmentVariable('PATH', $newPath, 'User')
     Write-Host "Removed from PATH: " -ForegroundColor Magenta -NoNewline; Write-Host $Path.TrimEnd('\')
 } else {
-    # Add normalized path
     $newPath = ($arrPath + $Path.TrimEnd('\')) -join [IO.Path]::PathSeparator
-    [System.Environment]::SetEnvironmentVariable('PATH', $newPath, 'User')
     Write-Host "Added to PATH: " -ForegroundColor Green -NoNewline; Write-Host $Path.TrimEnd('\')
 }
-
+[System.Environment]::SetEnvironmentVariable('PATH', $newPath, 'User')
 sleep 2
