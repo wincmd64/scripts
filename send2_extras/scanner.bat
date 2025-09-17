f12aa3e6cf98be832d7e19eb4820c6121316859d
sleepwalker <104612222+wincmd64@users.noreply.github.com>
2025-09-17 14:12:36 +0300

    Add files via upload

--- send2_extras/scanner.bat
+++ send2_extras/scanner.bat
@@ -1,4 +1,4 @@
-:: Wrapper for Emsisoft CLI Scanner â€” anti-malware scanning tool
+:: Wrapper for Emsisoft CLI Scanner — anti-malware scanning tool
 :: by github.com/wincmd64
 
 :: Usage:
@@ -12,6 +12,7 @@
 for /f "tokens=* delims=" %%a in ('where a2cmd.exe 2^>nul') do set "app=%%a"
 if not defined app if exist "%~dp0a2cmd.exe" set "app=%~dp0a2cmd.exe"
 if not exist "%app%" (echo. & echo  "a2cmd.exe" not found. & echo  Download it from: https://dl.emsisoft.com/EmsisoftCommandlineScanner64.exe & echo. & pause & exit) else (TITLE %app%)
+set "prm=/a /pup /cloud=0"
 
 :: arguments
 if /i "%~1"=="/s" (if "%~2"=="" goto :shortcut)
@@ -43,17 +44,23 @@ set count=0
 for %%A in (%*) do set /a count+=1
 echo.
 if %count% equ 0 (echo  [1] Scan: ^(nothing selected^)) else if %count% equ 1 (echo  [1] Scan: %*) else (echo  [1] Scan: %count% objects)
-echo  [2] Scan memory
-echo  [3] Show status
-echo  [4] Update signature
+if "%prm%"=="" (echo  [2] Change parameters, current: n\a) else (echo  [2] Change parameters, current: %prm%)
+echo  [3] Scan memory
+echo  [4] Show status
+echo  [5] Update signature
+echo  [0] Exit
 echo. 
 if %count% equ 0 (
-    CHOICE /C 234 /M "Your choice?:" >nul 2>&1
+    CHOICE /C 23450 /M "Your choice?:" >nul 2>&1
+    if errorlevel 5 goto exit
+    if errorlevel 4 goto Option_5
     if errorlevel 3 goto Option_4
     if errorlevel 2 goto Option_3
     if errorlevel 1 goto Option_2
 ) else (
-    CHOICE /C 1234 /M "Your choice?:" >nul 2>&1
+    CHOICE /C 123450 /M "Your choice?:" >nul 2>&1
+    if errorlevel 6 goto exit
+    if errorlevel 5 goto Option_5
     if errorlevel 4 goto Option_4
     if errorlevel 3 goto Option_3
     if errorlevel 2 goto Option_2
@@ -72,19 +79,27 @@ FOR %%k IN (%*) DO (
 if %found%==1 (color C) else (color A)
 echo. & echo  [DONE] & echo. & pause & exit
 :processFile
-echo. & echo  [%1/%2] "%~3"
-"%app%" "%~3" /a /pup /cloud=0
+echo. & echo  [%1/%2] a2cmd.exe "%~3" %prm%
+"%app%" "%~3" %prm%
 exit /b
 
 :Option_2
+echo  "%app%" %prm%
+echo.
+set "prm="
+set /p prm=Enter new parameters (for example /log=filepath): 
+if not defined prm (set "prm=")
+goto main
+
+:Option_3
 "%app%" /m
 pause & goto main
 
-:Option_3
+:Option_4
 "%app%" /status
 echo. & pause & goto main
 
-:Option_4
+:Option_5
 "%app%" /u
 echo. & pause & goto main
 
