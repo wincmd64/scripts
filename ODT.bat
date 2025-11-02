c5dcef3c10c01afdb5379521f0803fe4935378ac
sleepwalker <104612222+wincmd64@users.noreply.github.com>
2025-11-02 12:07:21 +0200

    Add files via upload

--- ODT.bat
+++ ODT.bat
@@ -1,49 +1,59 @@
 :: Office Deployment Tool
-::   You need to specify YOUR configuration .xml for XML_SOURCE
-::   Both local files and web links are supported.
-::   You can use https://config.office.com/deploymentsettings to configure .xml 
 :: by github.com/wincmd64
 
 @echo off
 chcp 1251 >nul
 (Net session >nul 2>&1)&&(cd /d "%~dp0")||(PowerShell start """%~0""" -verb RunAs & Exit /B)
 for /F "tokens=3 delims=." %%O in ('reg query "HKCR\Word.Application\CurVer" 2^>nul') do set officeVer=%%O
-if defined officeVer (TITLE Office v%officeVer% detected) else (TITLE Office not found)
-
-:: ===== USER XML =====
-:: This example for Office 2021 x64 ru
-set "XML_SOURCE=https://raw.githubusercontent.com/wincmd64/blog/refs/heads/main/office2021std.xml"
-:: ====================
+if defined officeVer (TITLE Office v%officeVer% detected) else (TITLE Office Deployment Tool)
 
 :: get downloads folder path
 for /f "delims=" %%a in ('powershell -NoP -C "(New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path"') do set "DOWNLOADS=%%a"
 
-pushd "%DOWNLOADS%"
-if not exist "setup.exe" (
-    echo. & echo  download "setup.exe" to "%DOWNLOADS%" ? & echo. & pause
-    curl.exe --ssl-no-revoke https://officecdn.microsoft.com/pr/wsus/setup.exe -OR#
-    if errorlevel 1 (color C & echo. & echo  ERROR: setup.exe download failed. & echo. & pause & exit)
-)
+echo. & echo  This script runs MS Office setup 
+echo  using an XML configuration file.
+echo.
+echo   [1] select XML file
+echo       ^(can be generated with 'https://config.office.com/deploymentsettings'^)
+echo   [2] use built-in preset: Office 2021 VL x64
+echo       ^(Word, Excel, PowerPoint only^)
+echo.
+CHOICE /C 12 /M "Your choice?:" >nul 2>&1
+if errorlevel 2 goto Option_2
+if errorlevel 1 goto Option_1
+exit
 
-:: web
-if "%XML_SOURCE:~0,7%"=="http://" goto DOWNLOAD_CONFIG
-if "%XML_SOURCE:~0,8%"=="https://" goto DOWNLOAD_CONFIG
-
-:: local
-if not exist "%XML_SOURCE%" (color C & echo. & echo  ERROR: "%XML_SOURCE%" not found. & echo. & pause & exit)
-set "CONFIG_FILE=%XML_SOURCE%"
+:Option_1
+for /f "delims=" %%A in ('powershell -NoP "Add-Type -AssemblyName System.Windows.Forms; $dlg=New-Object System.Windows.Forms.OpenFileDialog; $dlg.Filter='XML files (*.xml)|*.xml'; if($dlg.ShowDialog() -eq 'OK'){ $dlg.FileName }"') do set "XML_SOURCE=%%A"
+if not defined XML_SOURCE echo  Cancelled. & echo. & pause & exit
+echo  Selected: %XML_SOURCE% & echo.
 goto :run
 
-:DOWNLOAD_CONFIG
-for %%a in ("%XML_SOURCE%") do set "CONFIG_FILE=%%~nxa"
-if not exist "%CONFIG_FILE%" (
-    echo. & echo  download "%CONFIG_FILE%" ? & echo. & pause
-    curl.exe --ssl-no-revoke "%XML_SOURCE%" -#O
-    if errorlevel 1 (color C & echo. & echo  ERROR: xml config download failed. & echo. & pause & exit)
+:Option_2
+set "XML_SOURCE=%temp%\office2021.xml"
+> "%XML_SOURCE%" (
+  echo ^<Configuration^>
+  echo   ^<Add OfficeClientEdition="64" Channel="PerpetualVL2021"^>
+  echo     ^<Product ID="Standard2021Volume"^>
+  echo       ^<Language ID="MatchOS" /^>
+  echo       ^<ExcludeApp ID="OneDrive" /^>
+  echo       ^<ExcludeApp ID="OneNote" /^>
+  echo       ^<ExcludeApp ID="Outlook" /^>
+  echo       ^<ExcludeApp ID="Publisher" /^>
+  echo     ^</Product^>
+  echo   ^</Add^>
+  echo   ^<Display Level="Full" AcceptEULA="TRUE" /^>
+  echo ^</Configuration^>
 )
 
 :RUN
-echo. & echo  run: setup.exe /configure "%CONFIG_FILE%" ? & echo. & pause
-start "" "%DOWNLOADS%\setup.exe" /configure "%CONFIG_FILE%"
+pushd "%DOWNLOADS%"
+if not exist "setup.exe" (
+    echo  Download "setup.exe" to "%DOWNLOADS%" ? & echo. & pause
+    curl.exe --ssl-no-revoke https://officecdn.microsoft.com/pr/wsus/setup.exe -OR#
+    if errorlevel 1 (color C & echo. & echo  ERROR: setup.exe download failed. & echo. & pause & exit)
+)
+echo. & echo  run: setup.exe /configure "%XML_SOURCE%" ? & echo. & pause
+start "" "%DOWNLOADS%\setup.exe" /configure "%XML_SOURCE%"
 echo. & echo  Installation started...
 timeout 3
