8b2d0d445290a66479e8a0605b203ccdffae058c
sleepwalker <104612222+wincmd64@users.noreply.github.com>
2026-01-16 11:27:14 +0200

    Add files via upload

--- ODT.bat
+++ ODT.bat
@@ -3,18 +3,15 @@
 
 @echo off
 chcp 1251 >nul
-
 (Net session >nul 2>&1)&&(cd /d "%~dp0")||(PowerShell start """%~0""" -verb RunAs & Exit /B)
-for /F "tokens=3 delims=." %%O in ('reg query "HKCR\Word.Application\CurVer" 2^>nul') do set officeVer=%%O
-:: https://en.wikipedia.org/wiki/Microsoft_Office#History_of_releases
-if defined officeVer (TITLE Office v%officeVer% detected) else (TITLE Office Deployment Tool)
 
+:: ver finder https://en.wikipedia.org/wiki/Microsoft_Office#History_of_releases
+for /F "tokens=3 delims=." %%O in ('reg query "HKCR\Word.Application\CurVer" 2^>nul') do set officeVer=%%O
+if defined officeVer (TITLE Office v%officeVer% detected) else (TITLE Office Deployment Tool)
 :: get downloads folder path
 for /f "delims=" %%a in ('powershell -NoP -C "(New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path"') do set "DOWNLOADS=%%a"
 
-echo. & echo  This script runs MS Office setup 
-echo  using an XML configuration file.
-echo.
+echo. & echo   This script runs MS Office setup using an XML configuration file. & echo.
 echo   [1] select XML file
 echo       ^(can be generated with 'https://config.office.com/deploymentsettings'^)
 echo   [2] use built-in preset: Office 2021 VL x64
