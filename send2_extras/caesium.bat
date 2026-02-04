0bbf5a9b00a419d59d8e1e0b0bc5a6821d3f4647
sleepwalker <104612222+wincmd64@users.noreply.github.com>
2026-02-04 14:29:18 +0200

    Add files via upload

--- send2_extras/caesium.bat
+++ send2_extras/caesium.bat
@@ -9,10 +9,38 @@
 :: /s - create shortcut in Shell:SendTo folder
 
 @echo off
+:start
 for /f "tokens=* delims=" %%a in ('where caesiumclt.exe 2^>nul') do set "app=%%a"
 if not defined app if exist "%~dp0caesiumclt.exe" set "app=%~dp0caesiumclt.exe"
-if not exist "%app%" (echo. & echo  "caesiumclt.exe" not found. & echo  Download it from: https://github.com/Lymphatus/caesium-clt & echo. & pause & exit) else (TITLE %app%)
+if exist "%app%" goto skip_download
+echo. & echo  "caesiumclt.exe" not found. & echo  Try to download it to "%~dp0" ? & echo. & pause
+:: getting the latest version via the GitHub API
+echo. & echo  Getting the latest version...
+set "ps_get_url=$r = Invoke-RestMethod -Uri 'https://api.github.com/repos/Lymphatus/caesium-clt/releases/latest'; $a = $r.assets | Where-Object { $_.name -like '*windows*.zip' } | Select-Object -First 1; echo $a.browser_download_url"
+set "ps_get_name=$r = Invoke-RestMethod -Uri 'https://api.github.com/repos/Lymphatus/caesium-clt/releases/latest'; $a = $r.assets | Where-Object { $_.name -like '*windows*.zip' } | Select-Object -First 1; echo $a.name"
+for /f "tokens=*" %%a in ('powershell -command "%ps_get_url%"') do set "url=%%a"
+for /f "tokens=*" %%a in ('powershell -command "%ps_get_name%"') do set "filename=%%a"
+if "%url%"=="" (echo  Error: Could not find download URL. & echo  Try manual: https://github.com/Lymphatus/caesium-clt/releases & pause & exit /b)
+if not exist "%temp%\%filename%" (
+    echo. & echo  Downloading: %filename%
+    powershell -command "Invoke-WebRequest -Uri '%url%' -OutFile '%temp%\%filename%'"
+) else (
+    echo. & echo  Downloading: %filename% ^(already in TEMP^)
+)
+echo. & echo  Extracting ...
+md "%temp%\caesium"
+if exist "%temp%\%filename%" (tar -xf "%temp%\%filename%" -C "%temp%\caesium" 2>nul) else (echo. & echo  %filename% not found. & echo. & pause)
+:: finds .exe
+set "found_exe="
+for /r "%temp%\caesium" %%F in (*caesium*clt.exe) do (set "found_exe=%%~fF")
+:: move it
+if defined found_exe (move /y "%found_exe%" "%~dp0" >nul) else (echo. & echo  Error: caesiumclt.exe not found inside the archive.)
+rd /s /q "%temp%\caesium"
+echo. & echo. & echo  DONE. & echo. & pause & goto start
 
+:skip_download
+cls
+TITLE %app%
 :: arguments
 if /i "%~1"=="/s" (if "%~2"=="" goto shortcut)
 
