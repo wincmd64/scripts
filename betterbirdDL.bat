69543ffa6da508492effbb65100653847f2f0992
sleepwalker <104612222+wincmd64@users.noreply.github.com>
2026-02-09 15:36:35 +0200

    Add files via upload

--- betterbirdDL.bat
+++ betterbirdDL.bat
@@ -6,11 +6,11 @@ cd /d "%~dp0"
 
 echo. & echo  Loading ...
 :: get link
-for /f "usebackq tokens=*" %%a in (`powershell -NoP -C "(Invoke-WebRequest 'https://www.betterbird.eu/downloads/getloc.php?os=win&lang=en-US&version=release&portable=true' -UseBasicParsing).Content.Trim()"`) do set "DL_URL=%%a"
+for /f "usebackq tokens=*" %%a in (`powershell -NoP -C "(Invoke-WebRequest 'https://www.betterbird.eu/downloads/getloc.php?os=win&lang=en-US&version=release&portable=true' -UseBasicParsing).Content.Trim()"`) do set "URL=%%a"
 :: get server filename
-for %%i in ("%DL_URL%") do set "filename=%%~nxi"
+for %%i in ("%URL%") do set "filename=%%~nxi"
 :: get server ver
-for /f "usebackq tokens=*" %%v in (`powershell -NoP -C "if ('%DL_URL%' -match '(\d+\.\d+\.\d+[-a-z0-9]*)') { $Matches[1] }"`) do set "VERSION=%%v"
+for /f "usebackq tokens=*" %%v in (`powershell -NoP -C "if ('%URL%' -match '(\d+\.\d+\.\d+[-a-z0-9]*)') { $Matches[1] }"`) do set "VERSION=%%v"
 :: get local ver
 if exist "core\betterbird.exe" (
     for /f "tokens=*" %%v in ('powershell -command "(Get-Item 'core\betterbird.exe').VersionInfo.ProductVersion.Trim()"') do set "current_version=%%v"
@@ -20,14 +20,14 @@ if exist "core\betterbird.exe" (
 cls
 echo. & echo  Latest version found: %version%
 echo  Current version: %current_version%
-echo. & echo  Download and update in "%~dp0" ? & echo. & pause
+if "%current_version%"=="Not installed" (echo. & echo   Download to "%~dp0" ? & echo. & pause) else (echo. & echo   Update ? & echo. & pause) 
 :: is running?
 :check_task
 tasklist /fi "imagename eq betterbird.exe" | find /i "betterbird.exe" >nul
 if not errorlevel 1 (echo. & echo  Betterbird is currently running. & echo  Please close Betterbird before updating. & echo. & pause & goto check_task)
 if not exist "%filename%" (
     echo. & echo  Downloading: %filename%
-    powershell -C "Invoke-WebRequest -Uri '%DL_URL%' -OutFile '%filename%'"
+    powershell -C "Invoke-WebRequest -Uri '%URL%' -OutFile '%filename%'"
 ) else (echo. & echo  %filename% already exist.)
 echo. & echo  Extracting ...
 if exist "%filename%" (tar -xf "%filename%") else (echo. & echo  %filename% not found. & echo. & pause)
