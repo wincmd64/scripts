171cc0ef8b1830ab1ea323dd0608c84d86779624
sleepwalker <104612222+wincmd64@users.noreply.github.com>
2026-04-22 14:01:38 +0300

    Add files via upload

--- uploader_Q.bat
+++ uploader_Q.bat
@@ -1,6 +1,10 @@
 :: Temporary file hoster
-::  Change "litterbox" to "0x0" in [SETTINGS] below if you want to use 0x0.st service
 :: by github.com/wincmd64
+
+::  Change service in [SETTINGS] below
+::    litterbox.catbox.moe -- 1 GB, 3 days
+::    x0.at -- 1 GB, 3 days min
+::    transfer.whalebone.io -- 250 MB, 7 days
 
 :: [USAGE]
 :: Create a shortcut to this .bat file in the Shell:SendTo folder
@@ -15,10 +19,9 @@ setlocal
 for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do set "ESC=%%b"
 
 :: [SETTINGS]
-:: Service: litterbox / 0x0
+:: Service options: litterbox / x0 / whalebone
 set "service=litterbox"
-:: Lifetime for Litterbox: 1h, 12h, 24h, 72h
-set "lifetime=72h"
+
 TITLE Temporary file hoster: %service%
 
 :: arguments
@@ -27,12 +30,15 @@ if /i "%~1"=="/s" (if "%~2"=="" goto shortcut)
 set count=0
 for %%A in (%*) do set /a count+=1
 if %count% equ 0 (echo. & echo  No objects selected & echo. & pause & exit)
+
 if %count% equ 1 (
     echo. & echo  File: %1
     if /i "%service%"=="litterbox" (
-        for /f "delims=" %%i in ('curl -# -F "reqtype=fileupload" -F "time=%lifetime%" -F "fileToUpload=@%~1" https://litterbox.catbox.moe/resources/internals/api.php') do (set "LNK=%%i")
-    ) else (
-        for /f "delims=" %%i in ('curl -# -F "file=@%~1" https://0x0.st') do (set "LNK=%%i")
+        for /f "delims=" %%i in ('curl -# -F "reqtype=fileupload" -F "time=72h" -F "fileToUpload=@%~1" https://litterbox.catbox.moe/resources/internals/api.php') do (set "LNK=%%i")
+    ) else if /i "%service%"=="x0" (
+        for /f "delims=" %%i in ('curl -# -F "file=@%~1" https://x0.at') do (set "LNK=%%i")
+    ) else if /i "%service%"=="whalebone" (
+        for /f "delims=" %%i in ('curl -# --upload-file "%~1" https://transfer.whalebone.io') do (set "LNK=%%i")
     )
     if not defined LNK (echo  Error: Failed to get link. & exit /b)
     call echo  Link: %ESC%[36m%%LNK%%%ESC%[0m
@@ -44,9 +50,11 @@ if %count% equ 1 (
     FOR %%k IN (%*) DO (
         echo. & echo  FILE: %%k
         if /i "%service%"=="litterbox" (
-            for /f "delims=" %%i in ('curl -# -F "reqtype=fileupload" -F "time=%lifetime%" -F "fileToUpload=@%%~k" https://litterbox.catbox.moe/resources/internals/api.php') do (echo  Link: %ESC%[36m%%i%ESC%[0m)
-        ) else (
-            for /f "delims=" %%i in ('curl -# -F "file=@%%~k" https://0x0.st') do (echo  Link:%ESC%[36m %%i%ESC%[0m)
+            for /f "delims=" %%i in ('curl -# -F "reqtype=fileupload" -F "time=72h" -F "fileToUpload=@%%~k" https://litterbox.catbox.moe/resources/internals/api.php') do (echo  Link: %ESC%[36m%%i%ESC%[0m)
+        ) else if /i "%service%"=="x0" (
+            for /f "delims=" %%i in ('curl -# -F "file=@%%~k" https://x0.at') do (echo  Link: %ESC%[36m%%i%ESC%[0m)
+        ) else if /i "%service%"=="whalebone" (
+            for /f "delims=" %%i in ('curl -# --upload-file "%%~k" https://transfer.whalebone.io') do (echo  Link: %ESC%[36m%%i%ESC%[0m)
         )
         for /f "tokens=*" %%i in ('certutil -hashfile "%%~k" MD5 ^| find /v "MD5" ^| find /v "CertUtil"') do (echo   MD5: %%i)
     )
