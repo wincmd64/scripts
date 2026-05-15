1b6de0a4fa92bebe5724ae4fe0815e8a90bd303a
sleepwalker <104612222+wincmd64@users.noreply.github.com>
2026-05-15 11:22:01 +0300

    Add files via upload

--- DirGet/KeePass_eget.bat
+++ DirGet/KeePass_eget.bat
@@ -4,15 +4,17 @@
 :: Look for KeePass.exe in the script directory.
 :: If present, check for updates; otherwise offer to download the latest version.
 
-:: [COMMAND LINE ARGUMENTS]
+::   COMMAND LINE ARGUMENTS:
 :: /a - associate with .kdbx files (admin rights required)
 
-:: [DEPENDENCIES] -- searching in PATH
-:: SetUserFTA.exe -- https://setuserfta.com/SetUserFTA.zip
+::   DEPENDENCIES (searching in PATH):
 :: eget.exe -- https://github.com/inherelab/eget/releases/latest/download/eget-windows-amd64.exe
+:: SetUserFTA.exe -- https://setuserfta.com/SetUserFTA.zip
 
 @echo off
 setlocal
+:: escape colors
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do set "ESC=%%b"
 
 :: [SETTINGS]
 set "name=KeePass"
@@ -20,10 +22,11 @@ set "app=KeePass.exe"
 set "dir=%~dp0"
 cd /d "%dir%"
 
+:: dependencies
+where eget.exe >nul 2>&1 || (echo. & echo    [ERROR] eget.exe not found in PATH & echo. & pause)
+
 :: arguments
 if exist "%app%" if /i "%~1"=="/a" goto associate
-:: escape colors
-for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do set "ESC=%%b"
 
 :: get local ver
 if exist "%app%" (
@@ -31,6 +34,7 @@ if exist "%app%" (
     for /f "tokens=*" %%v in ('powershell -command "(Get-Item '%app%').VersionInfo.ProductVersion.Trim()"') do set "current_version=%%v"
 ) else (echo. & echo  Download %name% to "%dir%" ? & echo. & pause & echo.)
 
+:: get server ver
 if defined current_version (
     echo. & echo  Getting server version... & echo.
     "eget.exe" query "sourceforge:keepass/KeePass 2.x"
@@ -45,6 +49,7 @@ echo. & echo  %ESC%[7mDONE.%ESC%[0m & timeout 3 & exit
 
 :associate
 (Net session >nul 2>&1)&&(cd /d "%dir%")||(PowerShell start """%~0""" -verb RunAs -ArgumentList '/a' & Exit /B)
+where SetUserFTA.exe >nul 2>&1 || (echo. & echo    [ERROR] SetUserFTA.exe not found in PATH & echo. & pause)
 assoc .kdbx=kpass2
 ftype kpass2="%dir%%app%" "%%1"
 reg add "HKCU\Software\Kolbicz IT\SetUserFTA" /v RunCount /t REG_DWORD /d 1 /f >nul
--- DirGet/qBittorrent_eget.bat
+++ DirGet/qBittorrent_eget.bat
@@ -8,12 +8,14 @@
 :: /a - associate with .torrent files (admin rights required)
 
 ::   DEPENDENCIES (searching in PATH):
-:: SetUserFTA.exe -- https://setuserfta.com/SetUserFTA.zip
 :: eget.exe -- https://github.com/inherelab/eget/releases/latest/download/eget-windows-amd64.exe
 :: 7z.exe with 7z.dll -- https://www.7-zip.org
+:: SetUserFTA.exe -- https://setuserfta.com/SetUserFTA.zip
 
 @echo off
 setlocal
+:: escape colors
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do set "ESC=%%b"
 
 :: [SETTINGS]
 set "name=qBittorrent"
@@ -21,10 +23,21 @@ set "app=qbittorrent.exe"
 set "dir=%~dp0"
 cd /d "%dir%"
 
+:: dependencies
+set "deps_missing="
+echo. & echo  Checking dependencies:
+call :check eget.exe
+call :check 7z.exe
+echo.
+if not defined deps_missing cls
+goto :continue
+:check
+where "%~1" >nul 2>&1 && (echo   [FOUND] %~1) || (echo   [MISSING] %~1 & set "deps_missing=1")
+exit /b
+:continue
+
 :: arguments
 if exist "%app%" if /i "%~1"=="/a" goto associate
-:: escape colors
-for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do set "ESC=%%b"
 
 :: get local ver
 if exist "%app%" (
@@ -40,16 +53,13 @@ if defined current_version (
 )
 
 :: download and unpack
-"eget.exe" dl --extract-all --asset "x64,setup,exe,^asc,^lt20" "sourceforge:qbittorrent/qbittorrent-win32"
-if exist "$PLUGINSDIR\" rd /s /q "$PLUGINSDIR\"
-if exist "translations\" rd /s /q "translations\"
-if exist "qt.conf" del /q "qt.conf"
-if exist "qbittorrent.pdb" del /q "qbittorrent.pdb"
 if not exist "profile\" (md "profile")
-echo. & echo  %ESC%[7mDONE.%ESC%[0m & timeout 3 & exit
+"eget.exe" dl --file "qbittorrent.exe" --asset "x64,setup,exe,^asc,^lt20" "sourceforge:qbittorrent/qbittorrent-win32"
+if %errorlevel% neq 0 (echo. & puase) else (echo. & echo  %ESC%[7mDONE.%ESC%[0m & timeout 3 & exit)
 
 :associate
 (Net session >nul 2>&1)&&(cd /d "%dir%")||(PowerShell start """%~0""" -verb RunAs -ArgumentList '/a' & Exit /B)
+where SetUserFTA.exe >nul 2>&1 || (echo. & echo    [ERROR] SetUserFTA.exe not found in PATH & echo. & pause)
 assoc .torrent=qbit
 ftype qbit="%dir%%app%" "%%1"
 reg add "HKCU\Software\Kolbicz IT\SetUserFTA" /v RunCount /t REG_DWORD /d 1 /f >nul
